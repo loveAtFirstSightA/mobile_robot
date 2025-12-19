ea56051d59f0a21191c4662db2e0bdcf1217186a
lio <lioSgr1205@gmail.com>
2025-12-19 15:37:30 +0800

    feature: support control robot with keyboard

---
+++ src/robot_teleop/robot_teleop/teleop.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python
+
+"""
+ Copyright 2025 Author lio
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ """
+
+
+import os
+import select
+import sys
+
+from geometry_msgs.msg import Twist
+import rclpy
+from rclpy.qos import QoSProfile
+
+if os.name == 'nt':
+    import msvcrt
+else:
+    import termios
+    import tty
+
+ROBOT_MAX_LIN_VEL = 0.22
+ROBOT_MAX_ANG_VEL = 2.84
+
+LIN_VEL_STEP_SIZE = 0.01
+ANG_VEL_STEP_SIZE = 0.1
+
+msg = """
+Control Your Mobile Robot!
+---------------------------
+Moving around:
+        w
+   a    s    d
+        x
+
+w/x : increase/decrease linear velocity (Robot : ~ 0.22)
+a/d : increase/decrease angular velocity (Robot : ~ 2.84)
+
+space key, s : force stop
+
+CTRL-C to quit
+"""
+
+e = """
+Communications Failed
+"""
+
+
+def get_key(settings):
+    if os.name == 'nt':
+        return msvcrt.getch().decode('utf-8')
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if rlist:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
+
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+
+def print_vels(target_linear_velocity, target_angular_velocity):
+    print('currently:\tlinear velocity {0}\t angular velocity {1} '.format(
+        target_linear_velocity,
+        target_angular_velocity))
+
+
+def make_simple_profile(output_vel, input_vel, slop):
+    if input_vel > output_vel:
+        output_vel = min(input_vel, output_vel + slop)
+    elif input_vel < output_vel:
+        output_vel = max(input_vel, output_vel - slop)
+    else:
+        output_vel = input_vel
+
+    return output_vel
+
+
+def constrain(input_vel, low_bound, high_bound):
+    if input_vel < low_bound:
+        input_vel = low_bound
+    elif input_vel > high_bound:
+        input_vel = high_bound
+    else:
+        input_vel = input_vel
+
+    return input_vel
+
+
+def check_linear_limit_velocity(velocity):
+    return constrain(velocity, -ROBOT_MAX_LIN_VEL, ROBOT_MAX_LIN_VEL)
+
+def check_angular_limit_velocity(velocity):
+    return constrain(velocity, -ROBOT_MAX_ANG_VEL, ROBOT_MAX_ANG_VEL)
+
+def main():
+    settings = None
+    if os.name != 'nt':
+        settings = termios.tcgetattr(sys.stdin)
+
+    rclpy.init()
+    qos = QoSProfile(depth=10)
+    node = rclpy.create_node('teleop_keyboard')
+    pub = node.create_publisher(Twist, '/R0/cmd_vel', qos)
+
+    status = 0
+    target_linear_velocity = 0.0
+    target_angular_velocity = 0.0
+    control_linear_velocity = 0.0
+    control_angular_velocity = 0.0
+
+    try:
+        print(msg)
+        while (1):
+            key = get_key(settings)
+            if key == 'w':
+                target_linear_velocity =\
+                    check_linear_limit_velocity(target_linear_velocity + LIN_VEL_STEP_SIZE)
+                status = status + 1
+                print_vels(target_linear_velocity, target_angular_velocity)
+            elif key == 'x':
+                target_linear_velocity =\
+                    check_linear_limit_velocity(target_linear_velocity - LIN_VEL_STEP_SIZE)
+                status = status + 1
+                print_vels(target_linear_velocity, target_angular_velocity)
+            elif key == 'a':
+                target_angular_velocity =\
+                    check_angular_limit_velocity(target_angular_velocity + ANG_VEL_STEP_SIZE)
+                status = status + 1
+                print_vels(target_linear_velocity, target_angular_velocity)
+            elif key == 'd':
+                target_angular_velocity =\
+                    check_angular_limit_velocity(target_angular_velocity - ANG_VEL_STEP_SIZE)
+                status = status + 1
+                print_vels(target_linear_velocity, target_angular_velocity)
+            elif key == ' ' or key == 's':
+                target_linear_velocity = 0.0
+                control_linear_velocity = 0.0
+                target_angular_velocity = 0.0
+                control_angular_velocity = 0.0
+                print_vels(target_linear_velocity, target_angular_velocity)
+            else:
+                if (key == '\x03'):
+                    break
+
+            if status == 20:
+                print(msg)
+                status = 0
+
+            control_linear_velocity = make_simple_profile(
+                control_linear_velocity,
+                target_linear_velocity,
+                (LIN_VEL_STEP_SIZE / 2.0))
+
+            control_angular_velocity = make_simple_profile(
+                control_angular_velocity,
+                target_angular_velocity,
+                (ANG_VEL_STEP_SIZE / 2.0))
+
+            twist = Twist()
+            twist.linear.x = control_linear_velocity
+            twist.linear.y = 0.0
+            twist.linear.z = 0.0
+
+            twist.angular.x = 0.0
+            twist.angular.y = 0.0
+            twist.angular.z = control_angular_velocity
+
+            pub.publish(twist)
+    except Exception as e:
+        print(e)
+
+    finally:
+        twist = Twist()
+        twist.linear.x = 0.0
+        twist.linear.y = 0.0
+        twist.linear.z = 0.0
+        twist.angular.x = 0.0
+        twist.angular.y = 0.0
+        twist.angular.z = 0.0
+        pub.publish(twist)
+        if os.name != 'nt':
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+
+if __name__ == '__main__':
+    main()
