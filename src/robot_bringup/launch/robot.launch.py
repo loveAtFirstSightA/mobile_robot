6b5826479f1b1961c586aaded31e21422713e3a9
lio <lioSgr1205@gmail.com>
2025-12-20 11:19:42 +0800

    feature: support lds-01 lidar && add robot startup script

---
+++ src/robot_bringup/launch/robot.launch.py
@@ -0,0 +1,93 @@
+"""
+ Copyright 2025 Author lio
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ """
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import ThisLaunchFileDir
+from launch_ros.actions import Node
+from launch_ros.actions import PushRosNamespace
+
+
+def generate_launch_description():
+    namespace = LaunchConfiguration('namespace', default='')
+
+    usb_port = LaunchConfiguration('usb_port', default='/dev/ttyACM0')
+
+    robot_param_dir = LaunchConfiguration(
+        'robot_param_dir',
+        default=os.path.join(
+            get_package_share_directory('robot_bringup'),
+            'param',
+            'robot.yaml'))
+
+    lidar_pkg_dir = LaunchConfiguration(
+        'lidar_pkg_dir',
+        default=os.path.join(get_package_share_directory('hls_lfcd_lds_driver'), 'launch'))
+  
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value=use_sim_time,
+            description='Use simulation (Gazebo) clock if true'),
+
+        DeclareLaunchArgument(
+            'usb_port',
+            default_value=usb_port,
+            description='Connected USB port with OpenCR'),
+
+        DeclareLaunchArgument(
+            'robot_param_dir',
+            default_value=robot_param_dir,
+            description='Full path to turtlebot3 parameter file to load'),
+
+        DeclareLaunchArgument(
+            'namespace',
+            default_value=namespace,
+            description='Namespace for nodes'),
+
+        PushRosNamespace(namespace),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [ThisLaunchFileDir(), '/robot_state_publisher.launch.py']),
+            launch_arguments={'use_sim_time': use_sim_time,
+                              'namespace': namespace}.items(),
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([lidar_pkg_dir, '/hlds_laser.launch.py']),
+            launch_arguments={'port': '/dev/ttyUSB0',
+                              'frame_id': 'base_scan',
+                              'namespace': namespace}.items(),
+        ),
+
+        Node(
+            package='robot_node',
+            executable='robot_ros',
+            parameters=[
+                robot_param_dir,
+                {'namespace': namespace}],
+            arguments=['-i', usb_port],
+            output='screen'),
+    ])
